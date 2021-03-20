bb171f468841ee9a6d4206f201a0b6479ac311b8
Davide Corradi <davide.corradi.dev@gmail.com>
2021-03-20 11:37:46 +0100

    Sound logic

--- roe_audio/src/sound.rs
+++ roe_audio/src/sound.rs
@@ -1,24 +1,28 @@
 use super::AudioFormat;
 
+use itertools::interleave;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Sound {
-    channels: Vec<Vec<u16>>,
+    left_channel: Vec<i16>,
+    right_channel: Vec<i16>,
     sample_rate: u32,
 }
 
 impl Sound {
     pub fn from_raw_data(data: &[u8], format: AudioFormat, sample_rate: u32) -> Self {
         assert!(data.len() % format.total_bytes_per_sample() as usize == 0);
-        let channels = match format {
+        let (left_channel, right_channel) = match format {
             AudioFormat::Mono8 => {
-                let channel = data
+                let channel: Vec<i16> = data
                     .into_iter()
-                    .map(|sample| *sample as u16 * 2u16)
+                    .map(|sample| (*sample as i16 - 128) * 2)
                     .collect();
-                vec![channel]
+                (channel.clone(), channel)
             }
             AudioFormat::Mono16 => {
-                vec![Vec::from(bytemuck::cast_slice(data))]
+                let channel = Vec::from(bytemuck::cast_slice(data));
+                (channel.clone(), channel)
             }
             AudioFormat::Stereo8 => {
                 let mut left_channel =
@@ -26,10 +30,10 @@ impl Sound {
                 let mut right_channel =
                     Vec::with_capacity(data.len() / format.channel_count() as usize);
                 for i in (0..data.len()).step_by(2) {
-                    left_channel.push(data[i] as u16 * 2u16);
-                    right_channel.push(data[i + 1] as u16 * 2u16);
+                    left_channel.push((data[i] as i16 - 128) * 2);
+                    right_channel.push((data[i + 1] as i16 - 128) * 2);
                 }
-                vec![left_channel, right_channel]
+                (left_channel, right_channel)
             }
             AudioFormat::Stereo16 => {
                 let data = bytemuck::cast_slice(data);
@@ -41,12 +45,19 @@ impl Sound {
                     left_channel.push(data[i]);
                     right_channel.push(data[i + 1]);
                 }
-                vec![left_channel, right_channel]
+                (left_channel, right_channel)
             }
         };
         Self {
-            channels,
+            left_channel,
+            right_channel,
             sample_rate,
         }
+    }
+
+    pub fn stereo16_raw_data(&self) -> Vec<u8> {
+        let left_channel = Vec::from(bytemuck::cast_slice::<_, u8>(&self.left_channel[..]));
+        let right_channel = Vec::from(bytemuck::cast_slice::<_, u8>(&self.right_channel[..]));
+        interleave(left_channel.into_iter(), right_channel.into_iter()).collect()
     }
 }
