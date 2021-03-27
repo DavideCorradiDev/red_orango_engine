5316ded824406809f31146574199c486ba27d7be
Davide Corradi <davide.corradi.dev@gmail.com>
2021-03-27 10:47:58 +0100

    Static Source

--- roe_audio/src/context.rs
+++ roe_audio/src/context.rs
@@ -3,7 +3,8 @@ use lazy_static::lazy_static;
 use std::sync::Arc;
 
 pub use alto::{
-    AltoError as BackendError, AsBufferData, ContextAttrs as ContextDesc, Mono, SampleFrame, Stereo,
+    AltoError as BackendError, AsBufferData, ContextAttrs as ContextDesc, Mono, SampleFrame,
+    Source, SourceState, Stereo,
 };
 
 lazy_static! {
@@ -70,12 +71,61 @@ impl Buffer {
     pub fn new<F: SampleFrame, B: AsBufferData<F>>(
         context: &Context,
         data: B,
-        sample_rate: i32,
+        frequency: i32,
     ) -> Result<Self, BackendError> {
-        let buffer = context.value.new_buffer::<F, B>(data, sample_rate)?;
+        let buffer = context.value.new_buffer::<F, B>(data, frequency)?;
         Ok(Self {
             value: Arc::new(buffer),
         })
+    }
+}
+
+impl std::ops::Deref for Buffer {
+    type Target = alto::Buffer;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl std::fmt::Debug for Buffer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Buffer {{ }}")
+    }
+}
+
+pub struct StaticSource {
+    value: alto::StaticSource,
+}
+
+impl StaticSource {
+    pub fn new(context: &Context) -> Result<Self, BackendError> {
+        let static_source = context.value.new_static_source()?;
+        Ok(Self {
+            value: static_source,
+        })
+    }
+
+    pub fn set_buffer(&mut self, buf: &Buffer) -> Result<(), BackendError> {
+        self.value.set_buffer(Arc::clone(&buf.value))
+    }
+}
+
+impl std::ops::Deref for StaticSource {
+    type Target = alto::StaticSource;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl std::ops::DerefMut for StaticSource {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
+
+impl std::fmt::Debug for StaticSource {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "StaticSource {{ }}")
     }
 }
 
@@ -124,5 +174,34 @@ mod tests {
             },
         )
         .unwrap();
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn buffer_creation() {
+        let device = Device::default().unwrap();
+        let context = Context::default(&device).unwrap();
+        let buffer = Buffer::new::<Stereo<i16>, _>(&context, vec![12, 13, 14, 15], 5).unwrap();
+        expect_that!(&buffer.frequency(), eq(5));
+        expect_that!(&buffer.bits(), eq(16));
+        expect_that!(&buffer.channels(), eq(2));
+        expect_that!(&buffer.size(), eq(8));
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn static_source_creation() {
+        let device = Device::default().unwrap();
+        let context = Context::default(&device).unwrap();
+        let buffer = Buffer::new::<Stereo<i16>, _>(&context, vec![0; 256], 5).unwrap();
+        let mut source = StaticSource::new(&context).unwrap();
+        source.set_buffer(&buffer).unwrap();
+        expect_that!(&source.state(), eq(SourceState::Initial));
+        expect_that!(&source.gain(), close_to(1., 1e-6));
+        expect_that!(&source.min_gain(), close_to(0., 1e-6));
+        expect_that!(&source.max_gain(), close_to(1., 1e-6));
+        expect_that!(&source.reference_distance(), close_to(1., 1e-6));
+        expect_that!(&source.rolloff_factor(), close_to(1., 1e-6));
+        expect_that!(&source.pitch(), close_to(1., 1e-6));
     }
 }
