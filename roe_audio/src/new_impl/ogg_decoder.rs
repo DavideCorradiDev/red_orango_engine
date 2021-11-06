c0f1b3879415fd851a351b0c793a830a92f668a8
Davide Corradi <davide.corradi.dev@gmail.com>
2021-11-06 22:38:02 +0100

    Adding more implementation

--- roe_audio/src/new_impl/ogg_decoder.rs
+++ roe_audio/src/new_impl/ogg_decoder.rs
@@ -11,6 +11,7 @@ where
     comment_header: lewton::header::CommentHeader,
     setup_header: lewton::header::SetupHeader,
     stream_serial: u32,
+    format: AudioFormat,
 }
 
 impl<T> OggDecoder<T>
@@ -21,13 +22,45 @@ where
         let mut packet_reader = PacketReader::new(input);
         let ((ident_header, comment_header, setup_header), stream_serial) =
             lewton::inside_ogg::read_headers(&mut packet_reader)?;
+        const BYTES_PER_SAMPLE: u32 = 2;
+        let format = AudioFormat::new(ident_header.audio_channels as u32, BYTES_PER_SAMPLE);
         Ok(Self {
             packet_reader,
             ident_header,
             comment_header,
             setup_header,
             stream_serial,
+            format,
         })
+    }
+}
+
+impl<T> Decoder for OggDecoder<T>
+where
+    T: std::io::Read + std::io::Seek,
+{
+    fn audio_format(&self) -> AudioFormat {
+        self.format
+    }
+
+    fn byte_seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        Ok(0)
+    }
+
+    fn sample_rate(&self) -> u32 {
+        0
+    }
+
+    fn sample_count(&self) -> usize {
+        0
+    }
+
+    fn sample_stream_position(&mut self) -> std::io::Result<u64> {
+        Ok(0)
+    }
+
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        Ok(0)
     }
 }
 
@@ -87,7 +120,7 @@ impl From<lewton::header::HeaderReadError> for DecoderError {
     fn from(e: lewton::header::HeaderReadError) -> Self {
         match e {
             lewton::header::HeaderReadError::NotVorbisHeader => {
-                DecoderError::InvalidEncoding(String::from("Data is not in the vorbis format"))
+                DecoderError::InvalidEncoding(format!("{}", e))
             }
             _ => DecoderError::InvalidHeader(format!("{}", e)),
         }
@@ -98,6 +131,9 @@ impl From<ogg::reading::OggReadError> for DecoderError {
     fn from(e: ogg::reading::OggReadError) -> Self {
         match e {
             ogg::reading::OggReadError::ReadError(e) => DecoderError::IoError(e),
+            ogg::reading::OggReadError::NoCapturePatternFound => {
+                DecoderError::InvalidEncoding(format!("{}", e))
+            }
             _ => DecoderError::InvalidData(format!("{}", e)),
         }
     }
@@ -108,10 +144,28 @@ mod tests {
     use super::*;
     use galvanic_assert::{matchers::*, *};
 
+    // TODO: use DecoderError for wav decoder.
+    // TODO: match expectations in wav test.
     #[test]
     fn invalid_input_file() {
         let file = std::fs::File::open("data/audio/not-an-audio-file.txt").unwrap();
         let buf = std::io::BufReader::new(file);
-        expect_that!(&OggDecoder::new(buf), is_variant!(Result::Err));
+        let result = OggDecoder::new(buf);
+        expect_that!(&result, is_variant!(Result::Err));
+        if let Err(e) = result {
+            expect_that!(&e, is_variant!(DecoderError::InvalidEncoding));
+        }
+    }
+
+    #[test]
+    fn mono16_loading() {
+        let file = std::fs::File::open("data/audio/mono-16-44100.ogg").unwrap();
+        let buf = std::io::BufReader::new(file);
+        let decoder = OggDecoder::new(buf).unwrap();
+        expect_that!(&decoder.audio_format(), eq(AudioFormat::Mono16));
+        expect_that!(&decoder.byte_count(), eq(24 * 2));
+        expect_that!(&decoder.sample_count(), eq(24));
+        expect_that!(&decoder.byte_rate(), eq(44100 * 2));
+        expect_that!(&decoder.sample_rate(), eq(44100));
     }
 }
