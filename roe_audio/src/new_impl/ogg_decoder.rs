0193c4c5677b86d061ec16d9f752544b1da8619f
Davide Corradi <davide.corradi.dev@gmail.com>
2021-11-07 11:26:26 +0100

    Ogg implementation

--- roe_audio/src/new_impl/ogg_decoder.rs
+++ roe_audio/src/new_impl/ogg_decoder.rs
@@ -117,7 +117,8 @@ fn read_next_decoded_packet_interleaved<T: std::io::Read + std::io::Seek>(
     context: &mut OggContext,
 ) -> Result<Option<Vec<i16>>, DecoderError> {
     let decoded_packet = match read_next_decoded_packet_generic::<
-        T, lewton::samples::InterleavedSamples<_>,
+        T,
+        lewton::samples::InterleavedSamples<_>,
     >(packet_reader, context)?
     {
         Some(p) => p,
@@ -131,12 +132,7 @@ where
     T: std::io::Read + std::io::Seek,
 {
     packet_reader: PacketReader<T>,
-    ident_header: lewton::header::IdentHeader,
-    comment_header: lewton::header::CommentHeader,
-    setup_header: lewton::header::SetupHeader,
-    previous_window_right: lewton::audio::PreviousWindowRight,
-    cur_absgp: Option<u64>,
-    stream_serial: u32,
+    context: OggContext,
     format: AudioFormat,
     sample_count: usize,
 }
@@ -151,121 +147,32 @@ where
         let ((ident_header, comment_header, setup_header), stream_serial) =
             lewton::inside_ogg::read_headers(&mut packet_reader)?;
 
-        const BYTES_PER_SAMPLE: u32 = 2;
-        let format = AudioFormat::new(ident_header.audio_channels as u32, BYTES_PER_SAMPLE);
-
-        Ok(Self {
-            packet_reader,
+        let context = OggContext {
             ident_header,
             comment_header,
             setup_header,
             previous_window_right: lewton::audio::PreviousWindowRight::new(),
             cur_absgp: None,
             stream_serial,
+        };
+
+        const BYTES_PER_SAMPLE: u32 = 2;
+        let format = AudioFormat::new(context.ident_header.audio_channels as u32, BYTES_PER_SAMPLE);
+
+        Ok(Self {
+            packet_reader,
+            context,
             format,
             sample_count: 0,
         })
     }
 
-    fn decode_ogg_packet(&mut self, packet: &ogg::Packet) -> Result<Vec<Vec<i16>>, DecoderError> {
-        self.decode_ogg_packet_generic(packet)
-    }
-
-    fn decode_ogg_packet_generic<S: Samples>(
-        &mut self,
-        packet: &ogg::Packet,
-    ) -> Result<S, DecoderError> {
-        Ok(lewton::audio::read_audio_packet_generic(
-            &mut self.ident_header,
-            &mut self.setup_header,
-            &packet.data,
-            &mut self.previous_window_right,
-        )?)
-    }
-
-    fn read_next_ogg_packet(&mut self) -> Result<Option<ogg::Packet>, DecoderError> {
-        loop {
-            let packet = match self.packet_reader.read_packet()? {
-                Some(p) => p,
-                None => return Ok(None),
-            };
-
-            if packet.stream_serial() == self.stream_serial {
-                return Ok(Some(packet));
-            }
-
-            if packet.first_in_stream() {
-                // Re-initialize headers.
-                let ident_header = lewton::header::read_header_ident(&packet.data)?;
-
-                let packet = self.packet_reader.read_packet_expected()?;
-                let comment_header = lewton::header::read_header_comment(&packet.data)?;
-
-                let packet = self.packet_reader.read_packet_expected()?;
-                let setup_header = lewton::header::read_header_setup(
-                    &packet.data,
-                    ident_header.audio_channels,
-                    (ident_header.blocksize_0, ident_header.blocksize_1),
-                )?;
-
-                const BYTES_PER_SAMPLE: u32 = 2;
-                self.format =
-                    AudioFormat::new(ident_header.audio_channels as u32, BYTES_PER_SAMPLE);
-                self.ident_header = ident_header;
-                self.comment_header = comment_header;
-                self.setup_header = setup_header;
-                self.previous_window_right = lewton::audio::PreviousWindowRight::new();
-                self.cur_absgp = None;
-                self.stream_serial = packet.stream_serial();
-
-                // Read the first data packet to initialize the previous_window_right.
-                let packet = match self.packet_reader.read_packet()? {
-                    Some(p) => p,
-                    None => return Ok(None),
-                };
-                self.decode_ogg_packet(&packet)?;
-                self.cur_absgp = Some(packet.absgp_page());
-
-                return Ok(self.packet_reader.read_packet()?);
-            }
-        }
-    }
-
-    fn read_next_decoded_packet_generic<S: Samples>(&mut self) -> Result<Option<S>, DecoderError> {
-        let packet = match self.read_next_ogg_packet()? {
-            Some(p) => p,
-            None => return Ok(None),
-        };
-        let mut decoded_packet: S = self.decode_ogg_packet_generic(&packet)?;
-
-        // If this is the last packet in the logical bitstream, it has to be truncated so
-        // that the end matches the absgp of the current page.
-        if let (Some(absgp), true) = (self.cur_absgp, packet.last_in_stream()) {
-            let target_length = packet.absgp_page().saturating_sub(absgp) as usize;
-            decoded_packet.truncate(target_length);
-        }
-
-        if packet.last_in_page() {
-            self.cur_absgp = Some(packet.absgp_page());
-        } else if let &mut Some(ref mut absgp) = &mut self.cur_absgp {
-            *absgp += decoded_packet.num_samples() as u64;
-        }
-
-        Ok(Some(decoded_packet))
-    }
-
     fn read_next_decoded_packet(&mut self) -> Result<Option<Vec<Vec<i16>>>, DecoderError> {
-        self.read_next_decoded_packet()
+        read_next_decoded_packet(&mut self.packet_reader, &mut self.context)
     }
 
     fn read_next_decoded_packet_interleaved(&mut self) -> Result<Option<Vec<i16>>, DecoderError> {
-        let decoded_packet = match self
-            .read_next_decoded_packet_generic::<lewton::samples::InterleavedSamples<_>>()?
-        {
-            Some(p) => p,
-            None => return Ok(None),
-        };
-        Ok(Some(decoded_packet.samples))
+        read_next_decoded_packet_interleaved(&mut self.packet_reader, &mut self.context)
     }
 }
 
@@ -282,7 +189,7 @@ where
     }
 
     fn sample_rate(&self) -> u32 {
-        self.ident_header.audio_sample_rate
+        self.context.ident_header.audio_sample_rate
     }
 
     fn sample_count(&self) -> usize {
