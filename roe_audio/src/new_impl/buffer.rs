15fcd9a627ed3265f87c67b35c4ce94ecb645a24
Davide Corradi <davide.corradi.dev@gmail.com>
2021-11-08 23:51:22 +0100

    Test

--- roe_audio/src/new_impl/buffer.rs
+++ roe_audio/src/new_impl/buffer.rs
@@ -4,29 +4,15 @@ use alto::{Mono, Stereo};
 
 use std::sync::Arc;
 
-fn set_buffer_data_with_format(
-    buffer: &mut alto::Buffer,
-    data: &[u8],
-    format: AudioFormat,
-    frequency: i32,
-) -> Result<(), BackendError> {
-    match format {
-        AudioFormat::Mono8 => buffer.set_data::<Mono<u8>, _>(data, frequency),
-        AudioFormat::Stereo8 => buffer.set_data::<Stereo<u8>, _>(data, frequency),
-        AudioFormat::Mono16 => {
-            buffer.set_data::<Mono<i16>, _>(bytemuck::cast_slice::<u8, i16>(&data), frequency)
-        }
-        AudioFormat::Stereo16 => {
-            buffer.set_data::<Stereo<i16>, _>(bytemuck::cast_slice::<u8, i16>(&data), frequency)
-        }
-    }?;
-    Ok(())
-}
-
 pub struct Buffer {
+    // This member is wrapped inside an Arc because Alto::StaticSource requires so.
+    // This means that the value can't be modified anymore after creation.
+    // This ensures no race conditions, but also means that changing data in an existing buffer
+    // is not possible: a new buffer has to be created.
     value: Arc<alto::Buffer>,
 }
 
+// TODO: consider updating the interface to match that of decoder.
 impl Buffer {
     pub fn new(
         context: &Context,
@@ -49,7 +35,6 @@ impl Buffer {
         })
     }
 
-    // TODO: unit test.
     pub fn from_decoder<D: Decoder>(
         context: &Context,
         decoder: &mut D,
@@ -62,21 +47,6 @@ impl Buffer {
             decoder.audio_format(),
             decoder.sample_rate() as i32,
         )
-    }
-
-    // TODO: add a set_data function determining at runtime what AudioFormat to use.
-    pub fn set_data(&mut self, data: &[u8], format: AudioFormat, frequency: i32) -> Result<(), BackendError> {
-        match format {
-            AudioFormat::Mono8 => self.value.set_data::<Mono<u8>, _>(data, frequency),
-            AudioFormat::Stereo8 => self.value.set_data::<Stereo<u8>, _>(data, frequency),
-            AudioFormat::Mono16 => {
-                self.value.set_data::<Mono<i16>, _>(bytemuck::cast_slice::<u8, i16>(&data), frequency)
-            }
-            AudioFormat::Stereo16 => {
-                self.value.set_data::<Stereo<i16>, _>(bytemuck::cast_slice::<u8, i16>(&data), frequency)
-            }
-        }?;
-        Ok(())
     }
 }
 
@@ -95,13 +65,69 @@ impl std::fmt::Debug for Buffer {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{
+        super::{Device, WavDecoder},
+        *,
+    };
     use galvanic_assert::{matchers::*, *};
 
-    // TODO: test with different formats.
     #[test]
     #[serial_test::serial]
-    fn buffer_creation() {
+    fn mono8_buffer_creation() {
+        let device = Device::default().unwrap();
+        let context = Context::default(&device).unwrap();
+        let buffer = Buffer::new(
+            &context,
+            &[12, 13, 14, 15, 16, 17, 18, 19],
+            AudioFormat::Mono8,
+            5,
+        )
+        .unwrap();
+        expect_that!(&buffer.frequency(), eq(5));
+        expect_that!(&buffer.bits(), eq(8));
+        expect_that!(&buffer.channels(), eq(1));
+        expect_that!(&buffer.size(), eq(8));
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn mono16_buffer_creation() {
+        let device = Device::default().unwrap();
+        let context = Context::default(&device).unwrap();
+        let buffer = Buffer::new(
+            &context,
+            &[12, 13, 14, 15, 16, 17, 18, 19],
+            AudioFormat::Mono16,
+            5,
+        )
+        .unwrap();
+        expect_that!(&buffer.frequency(), eq(5));
+        expect_that!(&buffer.bits(), eq(16));
+        expect_that!(&buffer.channels(), eq(1));
+        expect_that!(&buffer.size(), eq(8));
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn stereo8_buffer_creation() {
+        let device = Device::default().unwrap();
+        let context = Context::default(&device).unwrap();
+        let buffer = Buffer::new(
+            &context,
+            &[12, 13, 14, 15, 16, 17, 18, 19],
+            AudioFormat::Stereo8,
+            5,
+        )
+        .unwrap();
+        expect_that!(&buffer.frequency(), eq(5));
+        expect_that!(&buffer.bits(), eq(8));
+        expect_that!(&buffer.channels(), eq(2));
+        expect_that!(&buffer.size(), eq(8));
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn stereo16_buffer_creation() {
         let device = Device::default().unwrap();
         let context = Context::default(&device).unwrap();
         let buffer = Buffer::new(
@@ -115,5 +141,22 @@ mod tests {
         expect_that!(&buffer.bits(), eq(16));
         expect_that!(&buffer.channels(), eq(2));
         expect_that!(&buffer.size(), eq(8));
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn creation_from_decoder() {
+        let file = std::fs::File::open("data/audio/stereo-16-44100.wav").unwrap();
+        let buf = std::io::BufReader::new(file);
+        let mut decoder = WavDecoder::new(buf).unwrap();
+
+        let device = Device::default().unwrap();
+        let context = Context::default(&device).unwrap();
+        let buffer = Buffer::from_decoder(&context, &mut decoder).unwrap();
+
+        expect_that!(&buffer.frequency(), eq(44100));
+        expect_that!(&buffer.bits(), eq(16));
+        expect_that!(&buffer.channels(), eq(2));
+        expect_that!(&buffer.size(), eq(84924));
     }
 }
