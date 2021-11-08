22879645633a340b498ac6ba05c23421640243c1
Davide Corradi <davide.corradi.dev@gmail.com>
2021-11-08 23:38:05 +0100

    WIP set data

--- roe_audio/src/new_impl/buffer.rs
+++ roe_audio/src/new_impl/buffer.rs
@@ -56,10 +56,28 @@ impl Buffer {
     ) -> Result<Self, BackendError> {
         // TODO: replace unwrap call. Needs wrapping error type.
         let data = decoder.read_all().unwrap();
-        Self::new(context, &data, decoder.audio_format(), decoder.sample_rate() as i32)
+        Self::new(
+            context,
+            &data,
+            decoder.audio_format(),
+            decoder.sample_rate() as i32,
+        )
     }
 
     // TODO: add a set_data function determining at runtime what AudioFormat to use.
+    pub fn set_data(&mut self, data: &[u8], format: AudioFormat, frequency: i32) -> Result<(), BackendError> {
+        match format {
+            AudioFormat::Mono8 => self.value.set_data::<Mono<u8>, _>(data, frequency),
+            AudioFormat::Stereo8 => self.value.set_data::<Stereo<u8>, _>(data, frequency),
+            AudioFormat::Mono16 => {
+                self.value.set_data::<Mono<i16>, _>(bytemuck::cast_slice::<u8, i16>(&data), frequency)
+            }
+            AudioFormat::Stereo16 => {
+                self.value.set_data::<Stereo<i16>, _>(bytemuck::cast_slice::<u8, i16>(&data), frequency)
+            }
+        }?;
+        Ok(())
+    }
 }
 
 impl std::ops::Deref for Buffer {
@@ -86,7 +104,13 @@ mod tests {
     fn buffer_creation() {
         let device = Device::default().unwrap();
         let context = Context::default(&device).unwrap();
-        let buffer = Buffer::new(&context, &[12, 13, 14, 15, 16, 17, 18, 19], AudioFormat::Stereo16, 5).unwrap();
+        let buffer = Buffer::new(
+            &context,
+            &[12, 13, 14, 15, 16, 17, 18, 19],
+            AudioFormat::Stereo16,
+            5,
+        )
+        .unwrap();
         expect_that!(&buffer.frequency(), eq(5));
         expect_that!(&buffer.bits(), eq(16));
         expect_that!(&buffer.channels(), eq(2));
