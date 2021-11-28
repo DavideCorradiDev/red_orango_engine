28f45c2f1f64b1a14272ae07aaad732c865fcc14
Davide Corradi <davide.corradi.dev@gmail.com>
2021-11-28 12:15:30 +0100

    DEV

--- roe_resource_manager/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub use std::io::Error as Error;
-
-mod resource_loader;
-pub use resource_loader::*;
-
-mod texture_loader;
-pub use texture_loader::*;
--- roe_resource_manager/src/resource_loader.rs
+++
@@ -1,6 +0,0 @@
-use super::Error;
-
-pub trait ResourceLoader<T> {
-    type Resource;
-    fn load<P: AsRef<std::path::Path>>(path: P) -> Result<Self::Resource, Error>;
-}
