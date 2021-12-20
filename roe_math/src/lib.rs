39c3294fa713467ad053644346e06b87a57315ee
Davide Corradi <davide.corradi.dev@gmail.com>
2021-12-20 17:28:53 +0100

    DEV

--- roe_math/src/lib.rs
+++ roe_math/src/lib.rs
@@ -2,11 +2,13 @@ pub use nalgebra::{
     base::*,
     geometry::{
         DualQuaternion, Point, Point1, Point2, Point3, Point4, Point5, Point6, Quaternion,
-        UnitDualQuaternion, UnitQuaternion,
+        UnitDualQuaternion, UnitQuaternion, UnitComplex
     },
     ComplexField, Field, RealField,
 };
 
+pub use Vector2 as Translation2;
+pub use UnitComplex as Rotation2;
 pub use Matrix3 as HomogeneousMatrix2;
 pub use Vector3 as HomogeneousVector2;
 pub use Matrix4 as HomogeneousMatrix3;
---
+++ roe_math/src/transform.rs
@@ -0,0 +1,46 @@
+use super::{HomogeneousMatrix2, RealField, Rotation2, Translation2};
+
+pub fn translation2<N: RealField + Copy>(translation: &Translation2<N>) -> HomogeneousMatrix2<N> {
+    let mut matrix = HomogeneousMatrix2::<N>::identity();
+    matrix[(0,2)] = translation[0];
+    matrix[(1,2)] = translation[1];
+    matrix
+}
+
+pub fn rotation2<N: RealField + Copy>(rotation: &Rotation2<N>) -> HomogeneousMatrix2<N> {
+    rotation.to_homogeneous()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use galvanic_assert::{matchers::*, *};
+
+    #[test]
+    fn test_translation2() {
+        let res = translation2(&Translation2::<f32>::new(2., 3.));
+        expect_that!(&res[(0,0)], close_to(1., 1e-6));
+        expect_that!(&res[(0,1)], close_to(0., 1e-6));
+        expect_that!(&res[(0,2)], close_to(2., 1e-6));
+        expect_that!(&res[(1,0)], close_to(0., 1e-6));
+        expect_that!(&res[(1,1)], close_to(1., 1e-6));
+        expect_that!(&res[(1,2)], close_to(3., 1e-6));
+        expect_that!(&res[(2,0)], close_to(0., 1e-6));
+        expect_that!(&res[(2,1)], close_to(0., 1e-6));
+        expect_that!(&res[(2,2)], close_to(1., 1e-6));
+    }
+
+    #[test]
+    fn test_rotation2() {
+        let res = rotation2(&Rotation2::<f32>::from_angle(1.));
+        expect_that!(&res[(0,0)], close_to(0.540302, 1e-6));
+        expect_that!(&res[(0,1)], close_to(-0.841471, 1e-6));
+        expect_that!(&res[(0,2)], close_to(0., 1e-6));
+        expect_that!(&res[(1,0)], close_to(0.841471, 1e-6));
+        expect_that!(&res[(1,1)], close_to(0.540302, 1e-6));
+        expect_that!(&res[(1,2)], close_to(0., 1e-6));
+        expect_that!(&res[(2,0)], close_to(0., 1e-6));
+        expect_that!(&res[(2,1)], close_to(0., 1e-6));
+        expect_that!(&res[(2,2)], close_to(1., 1e-6));
+    }
+}
