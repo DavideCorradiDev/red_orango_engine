d68fefb7d074ced612cba1f93cdfdaf6172500c2
Davide Corradi <davide.corradi.dev@gmail.com>
2021-12-20 17:54:58 +0100

    DEV

--- roe_math/src/lib.rs
+++ roe_math/src/lib.rs
@@ -1,17 +1,20 @@
 pub use nalgebra::{
     base::*,
+    convert, convert_ref, convert_ref_unchecked, convert_unchecked,
     geometry::{
         DualQuaternion, Point, Point1, Point2, Point3, Point4, Point5, Point6, Quaternion,
-        UnitDualQuaternion, UnitQuaternion, UnitComplex
+        UnitComplex, UnitDualQuaternion, UnitQuaternion,
     },
-    ComplexField, Field, RealField,
+    try_convert, try_convert_ref, ComplexField, Field, RealField,
 };
 
-pub use Vector2 as Translation2;
-pub use UnitComplex as Rotation2;
-pub use Vector2 as Scaling2;
 pub use Matrix3 as HomogeneousMatrix2;
+pub use UnitComplex as Rotation2;
+pub use Vector2 as Translation2;
+pub use Vector2 as Scale2;
+pub use Vector2 as Shear2;
 pub use Vector3 as HomogeneousVector2;
+
 pub use Matrix4 as HomogeneousMatrix3;
 pub use Vector4 as HomogeneousVector3;
 
--- roe_math/src/transform.rs
+++ roe_math/src/transform.rs
@@ -1,9 +1,9 @@
-use super::{HomogeneousMatrix2, RealField, Rotation2, Translation2, Scaling2};
+use super::{convert, HomogeneousMatrix2, RealField, Rotation2, Scale2, Shear2, Translation2};
 
 pub fn translation2<N: RealField + Copy>(translation: &Translation2<N>) -> HomogeneousMatrix2<N> {
     let mut matrix = HomogeneousMatrix2::<N>::identity();
-    matrix[(0,2)] = translation[0];
-    matrix[(1,2)] = translation[1];
+    matrix[(0, 2)] = translation[0];
+    matrix[(1, 2)] = translation[1];
     matrix
 }
 
@@ -11,11 +11,31 @@ pub fn rotation2<N: RealField + Copy>(rotation: &Rotation2<N>) -> HomogeneousMat
     rotation.to_homogeneous()
 }
 
-
-pub fn scaling2<N: RealField + Copy>(scaling: &Scaling2<N>) -> HomogeneousMatrix2<N> {
+pub fn scale2<N: RealField + Copy>(scale: &Scale2<N>) -> HomogeneousMatrix2<N> {
     let mut matrix = HomogeneousMatrix2::<N>::identity();
-    matrix[(0,0)] = scaling[0];
-    matrix[(1,1)] = scaling[1];
+    matrix[(0, 0)] = scale[0];
+    matrix[(1, 1)] = scale[1];
+    matrix
+}
+
+pub fn shear2<N: RealField + Copy>(shear: &Shear2<N>) -> HomogeneousMatrix2<N> {
+    let mut matrix = HomogeneousMatrix2::<N>::identity();
+    matrix[(0, 1)] = shear[0];
+    matrix[(1, 0)] = shear[1];
+    matrix
+}
+
+pub fn ortographic_projection2<N: RealField + Copy>(
+    left: N,
+    right: N,
+    bottom: N,
+    top: N,
+) -> HomogeneousMatrix2<N> {
+    let mut matrix = HomogeneousMatrix2::<N>::identity();
+    matrix[(0, 0)] = convert::<_, N>(2.0) / (right - left);
+    matrix[(0, 2)] = -(right + left) / (right - left);
+    matrix[(1, 1)] = convert::<_, N>(2.0) / (top - bottom);
+    matrix[(1, 2)] = -(top + bottom) / (top - bottom);
     matrix
 }
 
@@ -27,42 +47,70 @@ mod tests {
     #[test]
     fn test_translation2() {
         let res = translation2(&Translation2::<f32>::new(2., 3.));
-        expect_that!(&res[(0,0)], close_to(1., 1e-6));
-        expect_that!(&res[(0,1)], close_to(0., 1e-6));
-        expect_that!(&res[(0,2)], close_to(2., 1e-6));
-        expect_that!(&res[(1,0)], close_to(0., 1e-6));
-        expect_that!(&res[(1,1)], close_to(1., 1e-6));
-        expect_that!(&res[(1,2)], close_to(3., 1e-6));
-        expect_that!(&res[(2,0)], close_to(0., 1e-6));
-        expect_that!(&res[(2,1)], close_to(0., 1e-6));
-        expect_that!(&res[(2,2)], close_to(1., 1e-6));
+        expect_that!(&res[(0, 0)], close_to(1., 1e-6));
+        expect_that!(&res[(0, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(0, 2)], close_to(2., 1e-6));
+        expect_that!(&res[(1, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(1, 1)], close_to(1., 1e-6));
+        expect_that!(&res[(1, 2)], close_to(3., 1e-6));
+        expect_that!(&res[(2, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 2)], close_to(1., 1e-6));
     }
 
     #[test]
     fn test_rotation2() {
         let res = rotation2(&Rotation2::<f32>::from_angle(1.));
-        expect_that!(&res[(0,0)], close_to(0.540302, 1e-6));
-        expect_that!(&res[(0,1)], close_to(-0.841471, 1e-6));
-        expect_that!(&res[(0,2)], close_to(0., 1e-6));
-        expect_that!(&res[(1,0)], close_to(0.841471, 1e-6));
-        expect_that!(&res[(1,1)], close_to(0.540302, 1e-6));
-        expect_that!(&res[(1,2)], close_to(0., 1e-6));
-        expect_that!(&res[(2,0)], close_to(0., 1e-6));
-        expect_that!(&res[(2,1)], close_to(0., 1e-6));
-        expect_that!(&res[(2,2)], close_to(1., 1e-6));
+        expect_that!(&res[(0, 0)], close_to(0.540302, 1e-6));
+        expect_that!(&res[(0, 1)], close_to(-0.841471, 1e-6));
+        expect_that!(&res[(0, 2)], close_to(0., 1e-6));
+        expect_that!(&res[(1, 0)], close_to(0.841471, 1e-6));
+        expect_that!(&res[(1, 1)], close_to(0.540302, 1e-6));
+        expect_that!(&res[(1, 2)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 2)], close_to(1., 1e-6));
     }
 
     #[test]
-    fn test_scaling2() {
-        let res = scaling2(&Scaling2::<f32>::new(2., 3.));
-        expect_that!(&res[(0,0)], close_to(2., 1e-6));
-        expect_that!(&res[(0,1)], close_to(0., 1e-6));
-        expect_that!(&res[(0,2)], close_to(0., 1e-6));
-        expect_that!(&res[(1,0)], close_to(0., 1e-6));
-        expect_that!(&res[(1,1)], close_to(3., 1e-6));
-        expect_that!(&res[(1,2)], close_to(0., 1e-6));
-        expect_that!(&res[(2,0)], close_to(0., 1e-6));
-        expect_that!(&res[(2,1)], close_to(0., 1e-6));
-        expect_that!(&res[(2,2)], close_to(1., 1e-6));
+    fn test_scale2() {
+        let res = scale2(&Scale2::<f32>::new(2., 3.));
+        expect_that!(&res[(0, 0)], close_to(2., 1e-6));
+        expect_that!(&res[(0, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(0, 2)], close_to(0., 1e-6));
+        expect_that!(&res[(1, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(1, 1)], close_to(3., 1e-6));
+        expect_that!(&res[(1, 2)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 2)], close_to(1., 1e-6));
+    }
+
+    #[test]
+    fn test_shear2() {
+        let res = shear2(&Shear2::<f32>::new(2., 3.));
+        expect_that!(&res[(0, 0)], close_to(1., 1e-6));
+        expect_that!(&res[(0, 1)], close_to(2., 1e-6));
+        expect_that!(&res[(0, 2)], close_to(0., 1e-6));
+        expect_that!(&res[(1, 0)], close_to(3., 1e-6));
+        expect_that!(&res[(1, 1)], close_to(1., 1e-6));
+        expect_that!(&res[(1, 2)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 2)], close_to(1., 1e-6));
+    }
+
+    #[test]
+    fn test_ortographic_projection2() {
+        let res = ortographic_projection2::<f32>(1., 5., 2., 11.);
+        expect_that!(&res[(0, 0)], close_to(0.5, 1e-6));
+        expect_that!(&res[(0, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(0, 2)], close_to(-1.5, 1e-6));
+        expect_that!(&res[(1, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(1, 1)], close_to(0.222222, 1e-6));
+        expect_that!(&res[(1, 2)], close_to(-1.444444, 1e-6));
+        expect_that!(&res[(2, 0)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 1)], close_to(0., 1e-6));
+        expect_that!(&res[(2, 2)], close_to(1., 1e-6));
     }
 }
